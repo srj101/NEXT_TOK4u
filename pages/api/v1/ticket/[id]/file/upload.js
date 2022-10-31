db266f5e8e98541b5c6584647bfffe9252deb9da
srj101 <salim15-3117@diu.edu.bd>
2022-10-31 20:45:56 +0600

    Init

--- pages/api/v1/ticket/[id]/file/upload.js
+++ pages/api/v1/ticket/[id]/file/upload.js
@@ -3,9 +3,9 @@ const { prisma } = require("../../../../../../prisma/prisma");
 import { getSession } from "next-auth/react";
 import { IncomingForm } from "formidable";
 import fs from "fs";
-import open from 'open'
+import open from "open";
 import { createNecessaryDirectoriesSync } from "filesac";
-import { oAuth, ytconfig,  } from "./oauth2callback";
+import { oAuth, ytconfig } from "./oauth2callback";
 import youtube from "youtube-api";
 
 export const config = {
@@ -14,17 +14,16 @@ export const config = {
   },
 };
 
-
 export default async function UploadFile(req, res) {
   //   const session = await getSession({ req });
-  
+
   const { id } = req.query;
-console.log('from upload',id);
-  const  uploadPath = `./public/storage/tickets/${id}`;
+  console.log("from upload", id);
+  const uploadPath = `./storage/tickets/${id}`;
   await createNecessaryDirectoriesSync(`${uploadPath}/x`);
 
   try {
-     const form =await new IncomingForm({
+    const form = await new IncomingForm({
       uploadDir: `./storage`,
       keepExtensions: true,
     });
@@ -39,47 +38,53 @@ console.log('from upload',id);
         if (err) throw err;
         console.log("Successfully renamed - AKA moved!");
 
-// console.log(files.file);
+        // console.log(files.file);
 
-const filename = f.originalFilename
-        let title= fields.title
-        let description= fields.details+ ' And the toktok ticket ID was: '+ id
-        let ticketid= id
+        const filename = f.originalFilename;
+        let title = fields.title;
+        let description =
+          fields.details + " And the toktok ticket ID was: " + id;
+        let ticketid = id;
 
-let mimetype= f.mimetype.split('/')[0]
-if (mimetype !=='image') {
+        let mimetype = f.mimetype.split("/")[0];
+        if (mimetype !== "image") {
+          const data = await prisma.youtubesettings.findUnique({
+            where: { id: 1 },
+          });
 
-  const data=  await prisma.youtubesettings.findUnique({
-    where: {id: 1}
-  })
-  
-  if (data.id) {
-     /*
+          if (data.id) {
+            /*
 Developer worked on youtube API : Rakibul
 web: https://github.com/Rakibul-Islam-GitHub
 order: https://www.fiverr.com/rakibul_cse21
 linkedin: https://www.linkedin.com/in/rakibul21
 email: rakibulislam.cse21@gmail.com
 */
-   
-     const oAuth =await youtube.authenticate({
-      type: 'oauth',
-      client_id: data.clientid? data.clientid : '451352924057-ca2m6d0ioeku2i153qndkkqmlf3mu58i.apps.googleusercontent.com',
-      client_secret: data.clientsecret,
-      redirect_url: 'http://localhost:3000/api/v1/ticket/1/file/oauth2callback'
-  })
 
-     open(oAuth.generateAuthUrl({
-      access_type: 'offline',
-      scope: 'https://www.googleapis.com/auth/youtube.upload',
-      state: JSON.stringify({
-          filename, title, description, ticketid
-      })
-  }))
-  }
-  
-}
-       
+            const oAuth = await youtube.authenticate({
+              type: "oauth",
+              client_id: data.clientid
+                ? data.clientid
+                : "451352924057-ca2m6d0ioeku2i153qndkkqmlf3mu58i.apps.googleusercontent.com",
+              client_secret: data.clientsecret,
+              redirect_url: `${process.env.BASE_URL}/api/v1/ticket/1/file/oauth2callback`,
+            });
+
+            open(
+              oAuth.generateAuthUrl({
+                access_type: "offline",
+                scope: "https://www.googleapis.com/auth/youtube.upload",
+                state: JSON.stringify({
+                  filename,
+                  title,
+                  description,
+                  ticketid,
+                }),
+              })
+            );
+          }
+        }
+
         try {
           await prisma.ticketFile
             .create({
@@ -104,4 +109,4 @@ email: rakibulislam.cse21@gmail.com
     res.status(500).json({ error });
   }
 }
-export {oAuth};
+export { oAuth };
