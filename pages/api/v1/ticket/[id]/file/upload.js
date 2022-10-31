57b93c07bdf026935fd6399941c0b206483e0da6
srj101 <salim15-3117@diu.edu.bd>
2022-10-31 20:30:24 +0600

    Init

---
+++ pages/api/v1/ticket/[id]/file/upload.js
@@ -0,0 +1,107 @@
+const { prisma } = require("../../../../../../prisma/prisma");
+
+import { getSession } from "next-auth/react";
+import { IncomingForm } from "formidable";
+import fs from "fs";
+import open from 'open'
+import { createNecessaryDirectoriesSync } from "filesac";
+import { oAuth, ytconfig,  } from "./oauth2callback";
+import youtube from "youtube-api";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+
+export default async function UploadFile(req, res) {
+  //   const session = await getSession({ req });
+  
+  const { id } = req.query;
+console.log('from upload',id);
+  const  uploadPath = `./public/storage/tickets/${id}`;
+  await createNecessaryDirectoriesSync(`${uploadPath}/x`);
+
+  try {
+     const form =await new IncomingForm({
+      uploadDir: `./storage`,
+      keepExtensions: true,
+    });
+
+    form.parse(req, (err, fields, files) => {
+      // console.log(files.file);
+      const f = files.file;
+
+      const u = `${uploadPath}/${f.originalFilename}`;
+
+      fs.rename(`./storage/${f.newFilename}`, u, async function (err) {
+        if (err) throw err;
+        console.log("Successfully renamed - AKA moved!");
+
+// console.log(files.file);
+
+const filename = f.originalFilename
+        let title= fields.title
+        let description= fields.details+ ' And the toktok ticket ID was: '+ id
+        let ticketid= id
+
+let mimetype= f.mimetype.split('/')[0]
+if (mimetype !=='image') {
+
+  const data=  await prisma.youtubesettings.findUnique({
+    where: {id: 1}
+  })
+  
+  if (data.id) {
+     /*
+Developer worked on youtube API : Rakibul
+web: https://github.com/Rakibul-Islam-GitHub
+order: https://www.fiverr.com/rakibul_cse21
+linkedin: https://www.linkedin.com/in/rakibul21
+email: rakibulislam.cse21@gmail.com
+*/
+   
+     const oAuth =await youtube.authenticate({
+      type: 'oauth',
+      client_id: data.clientid? data.clientid : '451352924057-ca2m6d0ioeku2i153qndkkqmlf3mu58i.apps.googleusercontent.com',
+      client_secret: data.clientsecret,
+      redirect_url: 'http://localhost:3000/api/v1/ticket/1/file/oauth2callback'
+  })
+
+     open(oAuth.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/youtube.upload',
+      state: JSON.stringify({
+          filename, title, description, ticketid
+      })
+  }))
+  }
+  
+}
+       
+        try {
+          await prisma.ticketFile
+            .create({
+              data: {
+                filename: f.originalFilename,
+                ticketId: Number(id),
+                path: u,
+              },
+            })
+            .then((err) => console.log(err));
+          return res
+            .status(200)
+            .json({ message: "File Uploaded", success: true });
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({ message: error, success: false });
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+}
+export {oAuth};
---
+++ pages/api/v1/ticket/[id]/update.js
@@ -0,0 +1,37 @@
+const { prisma } = require("../../../../../prisma/prisma");
+import { Novu } from "@novu/node";
+
+const novu = new Novu(process.env.NOVU_TOKEN);
+export default async function updateTicket(req, res) {
+  const { id } = req.query;
+
+  const { note, detail } = req.body;
+
+  try {
+    await prisma.ticket.update({
+      where: { id: Number(id) },
+      data: {
+        detail,
+        note,
+      },
+    });
+
+    await novu.trigger("answering-tickets-by-team", {
+      to: {
+        subscriberId: data.email,
+        email: data.email,
+      },
+      payload: {
+        name: data.name,
+        answer: data.note,
+        title: data.title,
+        detail: data.detail,
+      },
+    });
+
+    res.status(201).json({ success: true, message: "Ticket saved" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+}
